3de64388b225b2acee3ec8cf6b24f5a693101dbb
David Richer <david.richer@autodesk.com>
2020-02-26 10:31:01 -0500

    Get the nameserver from /etc/resolv.conf

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env sh
 set -eu
 
-envsubst '${S3_BUCKET}' < /etc/nginx/conf.d/s3_proxy.conf.template > /etc/nginx/conf.d/s3_proxy.conf
+export NAMESERVER=`cat /etc/resolv.conf | grep "nameserver" | awk '{print $2}' | tr '\n' ' '`
+
+echo "S3 bucket : ${S3_BUCKET}" 
+echo "nameserver: ${NAMESERVER}" 
+envsubst '${S3_BUCKET},${NAMESERVER}' < /etc/nginx/conf.d/s3_proxy.conf.template > /etc/nginx/conf.d/s3_proxy.conf
 
 exec "$@"
